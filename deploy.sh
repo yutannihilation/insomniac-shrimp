dd7d4898fc77bfe1042fc5fb0c46948bd71ee092
Travis CI <yutannihilation@example.com>
2015-10-17 08:26:37 +0900

    use the force, travis!

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ git config user.name "Travis CI"
 git config user.email "yutannihilation@example.com"
 
 cd _book
+git init
 git add .
 git commit -m "deploy to Github Pages"
 
-git push --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
+git push --quiet --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
